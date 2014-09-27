3ef7dc8c4ff0ecbae2521b70ec8f6a93324baefe
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-09-27 20:37:04 +0200

    WIP

---
+++ Tests/PrettyPrintNet.UnitsNet.Tests/PrintLengthTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace PrettyPrintNet.UnitsNet.Tests
+{
+    [TestFixture]
+    public class PrintLengthTests
+    {
+        [Test]
+        public void GetFeetAndInches()
+        {
+            Assert.AreEqual("6\" 2'", );
+        }
+}
+}
