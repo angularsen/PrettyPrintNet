af259cae83f64a4ebf0ee218f92a4553599dc296
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-09-28 00:43:31 +0200

    Code cleanup

--- Src/PrettyPrintNet/Properties/AssemblyInfo.cs
+++ Src/PrettyPrintNet/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("PrettyPrintNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -11,6 +12,5 @@
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.1.0")]
