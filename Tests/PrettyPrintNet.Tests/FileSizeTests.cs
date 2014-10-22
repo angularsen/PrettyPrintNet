30e4f2ea35e7490295f01156beb72e1721c4ad53
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-10-22 09:23:10 +0200

    test: Fix tests after changing unit base to 1000

--- Tests/PrettyPrintNet.Tests/FileSizeTests.cs
+++ Tests/PrettyPrintNet.Tests/FileSizeTests.cs
@@ -28,7 +28,7 @@ namespace PrettyPrintNet.Tests
         [Test]
         public void ReturnsKiloBytesUnder1MB()
         {
-            ulong KB = 1024;
+            ulong KB = 1000;
             Assert.AreEqual("1 kilobyte", FileSize.ToLongString(1 * KB, _enUsCulture));
             Assert.AreEqual("1 KB", FileSize.ToShortString(1 * KB, _enUsCulture));
             Assert.AreEqual("500 kilobytes", FileSize.ToLongString(500 * KB, _enUsCulture));
@@ -38,7 +38,7 @@ namespace PrettyPrintNet.Tests
         [Test]
         public void ReturnsMegaBytesUnder1GB()
         {
-            ulong MB = Convert.ToUInt64(Math.Pow(1024, 2));
+            ulong MB = Convert.ToUInt64(Math.Pow(1000, 2));
             Assert.AreEqual("1 megabyte", FileSize.ToLongString(1 * MB, _enUsCulture));
             Assert.AreEqual("1 MB", FileSize.ToShortString(1 * MB, _enUsCulture));
             Assert.AreEqual("500 megabytes", FileSize.ToLongString(500 * MB, _enUsCulture));
@@ -48,7 +48,7 @@ namespace PrettyPrintNet.Tests
         [Test]
         public void ReturnsGigaBytesUnder1TB()
         {
-            ulong GB = Convert.ToUInt64(Math.Pow(1024, 3));
+            ulong GB = Convert.ToUInt64(Math.Pow(1000, 3));
             Assert.AreEqual("1 gigabyte", FileSize.ToLongString(1 * GB, _enUsCulture));
             Assert.AreEqual("1 GB", FileSize.ToShortString(1 * GB, _enUsCulture));
             Assert.AreEqual("500 gigabytes", FileSize.ToLongString(500 * GB, _enUsCulture));
@@ -58,7 +58,7 @@ namespace PrettyPrintNet.Tests
         [Test]
         public void ReturnsTeraBytesUnder1PB()
         {
-            ulong TB = Convert.ToUInt64(Math.Pow(1024, 4));
+            ulong TB = Convert.ToUInt64(Math.Pow(1000, 4));
             Assert.AreEqual("1 terabyte", FileSize.ToLongString(1 * TB, _enUsCulture));
             Assert.AreEqual("1 TB", FileSize.ToShortString(1 * TB, _enUsCulture));
             Assert.AreEqual("500 terabytes", FileSize.ToLongString(500 * TB, _enUsCulture));
@@ -68,7 +68,7 @@ namespace PrettyPrintNet.Tests
         [Test]
         public void ReturnsPetaBytesUnder1EB()
         {
-            ulong PB = Convert.ToUInt64(Math.Pow(1024, 5));
+            ulong PB = Convert.ToUInt64(Math.Pow(1000, 5));
             Assert.AreEqual("1 petabyte", FileSize.ToLongString(1 * PB, _enUsCulture));
             Assert.AreEqual("1 PB", FileSize.ToShortString(1 * PB, _enUsCulture));
             Assert.AreEqual("500 petabytes", FileSize.ToLongString(500 * PB, _enUsCulture));
@@ -76,10 +76,10 @@ namespace PrettyPrintNet.Tests
         }
 
         [Test]
-        public void ReturnsExaBytesUnder1024EB()
+        public void ReturnsExaBytesUnder1000EB()
         {
             // 64-bit unsigned long can only hold 4.6 exabytes of bytes
-            ulong EB = Convert.ToUInt64(Math.Pow(1024, 6));
+            ulong EB = Convert.ToUInt64(Math.Pow(1000, 6));
             Assert.AreEqual("1 exabyte", FileSize.ToLongString(1 * EB, _enUsCulture));
             Assert.AreEqual("1 EB", FileSize.ToShortString(1 * EB, _enUsCulture));
             Assert.AreEqual("4 exabytes", FileSize.ToLongString(4 * EB, _enUsCulture));
@@ -89,7 +89,7 @@ namespace PrettyPrintNet.Tests
         [Test]
         public void DefaultStringFormatUseFewerDecimalsForLargerValues()
         {
-            ulong MB = Convert.ToUInt64(Math.Pow(1024, 2));
+            ulong MB = Convert.ToUInt64(Math.Pow(1000, 2));
             Assert.AreEqual("0 bytes", FileSize.ToLongString(0, _enUsCulture));
             Assert.AreEqual("1.23 megabytes", FileSize.ToLongString(Convert.ToUInt64(1.23456 * MB), _enUsCulture));
             Assert.AreEqual("10.2 megabytes", FileSize.ToLongString(Convert.ToUInt64(10.23456 * MB), _enUsCulture));
@@ -99,7 +99,7 @@ namespace PrettyPrintNet.Tests
         [Test]
         public void AppliesCustomStringFormatToValue()
         {
-            ulong MB = Convert.ToUInt64(Math.Pow(1024, 2));
+            ulong MB = Convert.ToUInt64(Math.Pow(1000, 2));
             const string stringFormat = "0.000";
             Assert.AreEqual("0.000 bytes", FileSize.ToLongString(0, _enUsCulture, stringFormat));
             Assert.AreEqual("1.235 megabytes", FileSize.ToLongString(Convert.ToUInt64(1.23456 * MB), _enUsCulture, stringFormat));
