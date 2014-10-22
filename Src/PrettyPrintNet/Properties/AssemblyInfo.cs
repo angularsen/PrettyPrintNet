82eaeefb8b24540450c12c031f5fe829be235d55
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-10-22 09:21:40 +0200

    Bump to 1.5

--- Src/PrettyPrintNet/Properties/AssemblyInfo.cs
+++ Src/PrettyPrintNet/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyFileVersion("1.4.0")]
+[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.5.0")]
